0de7622e3a3543fd5134a20ca041cbc3ee2f0749
DeAndre Johnson <dre@phreshr.com>
2016-05-09 13:01:49 -0400

    Add comments

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,9 @@ app.use(bodyParser.json());
 function ngApp(req, res) {
   let baseUrl = '/';
   let url = req.originalUrl || '/';
-  
+
+  /////// ORIGINAL EXCEPTION: No provider for Http! ///////
+
   let bootloader = new Bootloader({
     template: `
     <!doctype html>
@@ -77,6 +79,49 @@ function ngApp(req, res) {
 
   bootloader.serializeApplication()
   .then(html => console.log(html));
+
+  /////// THIS METHOD WORKS ///////
+
+  // let config: BootloaderConfig = {
+  //   template: `
+  //   <!doctype html>
+  //   <html>
+  //     <head>
+  //       <title>Angular 2 Universal Starter</title>
+  //       <meta charset="UTF-8">
+  //       <meta name="description" content="Angular 2 Universal">
+  //       <meta name="keywords" content="Angular 2,Universal">
+  //       <meta name="author" content="PatrickJS">
+
+  //       <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+
+  //       <base href="/">
+  //     </head>
+  //     <body>
+  //       <app>... Loading Universal ...</app>
+  //       <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+  //       <script src="bundle.js"></script>
+  //     </body>
+  //   </html>
+  //   `,
+  //   directives: [ App ],
+  //   platformProviders: [
+  //     provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
+  //     provide(BASE_URL, {useValue: baseUrl}),
+  //   ],
+  //   providers: [
+  //     provide(REQUEST_URL, {useValue: url}),
+  //     ...NODE_ROUTER_PROVIDERS,
+  //     ...NODE_HTTP_PROVIDERS
+  //   ],
+  //   async: true,
+  //   preboot: false // { appRoot: 'app' } // your top level app component selector
+  // };
+
+  // let renderHtml = config.template;
+  // res.send(renderHtml);
+
+  /////// DEFAULT ///////
 
   // let config: ExpressEngineConfig = {
   //   directives: [ App ],
