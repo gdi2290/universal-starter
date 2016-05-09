7c0103306ecab46e4fe7936071f823c28d1cfd29
DeAndre Johnson <dre@phreshr.com>
2016-05-09 12:40:58 -0400

    render output as string

--- src/server.ts
+++ src/server.ts
@@ -14,21 +14,23 @@ import {
   BASE_URL,
   NODE_ROUTER_PROVIDERS,
   NODE_HTTP_PROVIDERS,
-  ExpressEngineConfig
+  ExpressEngineConfig,
+  BootloaderConfig,
+  Bootloader
 } from 'angular2-universal';
 
 // Application
 import {App} from './app/app.component';
 
 const app = express();
-const ROOT = path.join(path.resolve(__dirname, '..'));
+// const ROOT = path.join(path.resolve(__dirname, '..'));
 
 enableProdMode();
 
 // Express View
-app.engine('.html', expressEngine);
-app.set('views', __dirname);
-app.set('view engine', 'html');
+// app.engine('.html', expressEngine);
+// app.set('views', __dirname);
+// app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 
@@ -36,31 +38,71 @@ app.use(bodyParser.json());
 function ngApp(req, res) {
   let baseUrl = '/';
   let url = req.originalUrl || '/';
+  
+  let bootloader = new Bootloader({
+    template: `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Angular 2 Universal Starter</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Angular 2 Universal">
+        <meta name="keywords" content="Angular 2,Universal">
+        <meta name="author" content="PatrickJS">
 
-  let config: ExpressEngineConfig = {
-    directives: [ App ],
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+
+        <base href="/">
+      </head>
+      <body>
+        <app>... Loading Universal ...</app>
+        <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+    `,
+    directives: [App],
     platformProviders: [
-      provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
-      provide(BASE_URL, {useValue: baseUrl}),
+      provide(ORIGIN_URL, { useValue: 'http://localhost:3000' }),
+      provide(BASE_URL, {useValue: baseUrl})
     ],
     providers: [
       provide(REQUEST_URL, {useValue: url}),
-      NODE_ROUTER_PROVIDERS,
-      NODE_HTTP_PROVIDERS,
+      ...NODE_ROUTER_PROVIDERS,
+      ...NODE_HTTP_PROVIDERS
     ],
     async: true,
-    preboot: false // { appRoot: 'app' } // your top level app component selector
-  };
+    preboot: false
+  });
 
-  res.render('index', config);
+  bootloader.serializeApplication()
+  .then(html => console.log(html));
+
+  // let config: ExpressEngineConfig = {
+  //   directives: [ App ],
+  //   platformProviders: [
+  //     provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
+  //     provide(BASE_URL, {useValue: baseUrl}),
+  //   ],
+  //   providers: [
+  //     provide(REQUEST_URL, {useValue: url}),
+  //     NODE_ROUTER_PROVIDERS,
+  //     NODE_HTTP_PROVIDERS,
+  //   ],
+  //   async: true,
+  //   preboot: false // { appRoot: 'app' } // your top level app component selector
+  // };
+
+  // res.render('index', config);
 }
 
-function indexFile(req, res) {
-  res.sendFile('/index.html', {root: __dirname});
-}
+// function indexFile(req, res) {
+//   res.sendFile('/index.html', {root: __dirname});
+// }
 
 // Serve static files
-app.use(express.static(ROOT, {index: false}));
+// app.use(express.static(ROOT, {index: false}));
+app.use(express.static('dist/client'));
 
 // Our API for demos only
 app.get('/data.json', (req, res) => {
